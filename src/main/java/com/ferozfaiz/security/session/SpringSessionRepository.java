5c83b3f4b4eb22d4d2d847c3f2a5a5ae7147eea1
Feroz <nartix@gmail.com>
2024-11-28 05:54:30 -0500

    implemented findBySessionId for session api

--- src/main/java/com/ferozfaiz/security/session/SpringSessionRepository.java
+++ src/main/java/com/ferozfaiz/security/session/SpringSessionRepository.java
@@ -1,8 +1,13 @@
 package com.ferozfaiz.security.session;
 
+import com.ferozfaiz.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "sessions", path = "sessions")
 public interface SpringSessionRepository extends JpaRepository<SpringSession, String> {
+    Optional<SpringSession> findBySessionId(@Param("sessionId") String sessionId);
 }
