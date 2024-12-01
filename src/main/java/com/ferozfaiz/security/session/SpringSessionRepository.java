ad0ad9614901554b25d6c9e3f7c14a56792b0df6
Feroz <nartix@gmail.com>
2024-12-01 09:04:25 -0500

    implemented deleteBySessionId

--- src/main/java/com/ferozfaiz/security/session/SpringSessionRepository.java
+++ src/main/java/com/ferozfaiz/security/session/SpringSessionRepository.java
@@ -2,12 +2,25 @@ package com.ferozfaiz.security.session;
 
 import com.ferozfaiz.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "sessions", path = "sessions")
 public interface SpringSessionRepository extends JpaRepository<SpringSession, String> {
     Optional<SpringSession> findBySessionId(@Param("sessionId") String sessionId);
+
+    // todo: only allow DELETE requests
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SpringSession s WHERE s.sessionId = :sessionId")
+    @RestResource(path = "deleteBySessionId", rel = "deleteBySessionId", exported = true)
+    void deleteBySessionId(@Param("sessionId") String sessionId);
+
 }
